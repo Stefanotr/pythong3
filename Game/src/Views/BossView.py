24580726204ff34002b834370d92822fc9539f8c
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2026-02-02 15:27:42 +0100

    BossModel and View

---
+++ Game/src/Views/BossView.py
@@ -0,0 +1,11 @@
+import pygame
+from Models.BossModel import BossModel
+
+class BossView():
+
+    def __init__(self):
+        boss_model=BossModel()
+        image_path = "Game/Assets/image.png"
+    
+
+    def afficherBoss():
