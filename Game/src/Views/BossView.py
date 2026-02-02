8ff153d3e943fd88b7ce3ee75854bc4cf62322b4
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2026-02-02 15:49:25 +0100

    Boss implementation

--- Game/src/Views/BossView.py
+++ Game/src/Views/BossView.py
@@ -1,11 +1,21 @@
 import pygame
 from Models.BossModel import BossModel
 
-class BossView():
+class BossView:
 
-    def __init__(self):
-        boss_model=BossModel()
-        image_path = "Game/Assets/image.png"
-    
+    def __init__(self, screen):
+        self.screen = screen
+        self.boss_model = BossModel()
 
-    def afficherBoss():
+       
+        self.image = pygame.image.load("Game/Assets/boss.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+        
+        self.rect.topleft = (
+            self.boss_model.position_x,
+            self.boss_model.position_y
+        )
+
+    def afficherBoss(self):
+        self.screen.blit(self.image, self.rect)
