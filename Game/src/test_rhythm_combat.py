468178bb5eda732dd67e01e6e5e43e6cae5476cb
SilenceWolf <66781009+SilenceWolf@users.noreply.github.com>
2026-02-04 21:15:39 +0100

    commit

--- Game/src/test_rhythm_combat.py
+++ Game/src/test_rhythm_combat.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 from Models.CaracterModel import CaracterModel
 from Models.RhythmModel import RhythmModel 
